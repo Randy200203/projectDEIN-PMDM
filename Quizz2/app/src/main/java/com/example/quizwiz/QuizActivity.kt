d297435fa5e530d8795af90829d862f281dd5bab
Randy.A <rachana@educacion.navarra.es>
2024-04-16 16:55:23 +0200

    improving on
    QuizWiz

--- Quizz2/app/src/main/java/com/example/quizwiz/QuizActivity.kt
+++ Quizz2/app/src/main/java/com/example/quizwiz/QuizActivity.kt
@@ -1,11 +1,17 @@
 package com.example.quizwiz
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import com.example.quizwiz.databinding.ActivityQuizBinding
+import com.example.quizwiz.databinding.ScoreDialogBinding
 
-class QuizActivity : AppCompatActivity() {
+class QuizActivity : AppCompatActivity(),View.OnClickListener {
 
     companion object  {
         var questionModelList : List<QuestionModel> = listOf()
@@ -15,10 +21,19 @@ class QuizActivity : AppCompatActivity() {
     lateinit var binding: ActivityQuizBinding
 
     var currentQuestionIndex = 0;
+    var selectedAns = ""
+    var score = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityQuizBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.apply {
+            btn0.setOnClickListener(this@QuizActivity)
+            btn1.setOnClickListener(this@QuizActivity)
+            btn2.setOnClickListener(this@QuizActivity)
+            btn3.setOnClickListener(this@QuizActivity)
+            nextBtn.setOnClickListener(this@QuizActivity)
+        }
         loadQuestions()
         startTimer()
     }
@@ -41,6 +56,11 @@ class QuizActivity : AppCompatActivity() {
     }
 
     private fun loadQuestions(){
+        selectedAns = ""
+        if (currentQuestionIndex == questionModelList.size){
+            finishQuiz()
+            return
+        }
 
         binding.apply {
             questionIndicatorTextview.text = "Question ${currentQuestionIndex+1}/ ${questionModelList.size}"
@@ -52,5 +72,55 @@ class QuizActivity : AppCompatActivity() {
             btn2.text = questionModelList[currentQuestionIndex].options[2]
             btn3.text = questionModelList[currentQuestionIndex].options[3]
         }
+    }
+
+    override fun onClick(view: View?) {
+
+        binding.apply {
+            btn0.setBackgroundColor(getColor(R.color.grey))
+            btn1.setBackgroundColor(getColor(R.color.grey))
+            btn2.setBackgroundColor(getColor(R.color.grey))
+            btn3.setBackgroundColor(getColor(R.color.grey))
+        }
+        val clickedBtn = view as Button
+        if (clickedBtn.id==R.id.next_btn){
+            //next button
+            if (selectedAns == questionModelList[currentQuestionIndex].correct){
+                score++
+                Log.i("Score of quiz", score.toString())
+            }
+            currentQuestionIndex++
+            loadQuestions()
+        }else{
+            //options is clicked
+            selectedAns = clickedBtn.text.toString()
+            clickedBtn.setBackgroundColor(getColor(R.color.orange))
+        }
+    }
+
+    private fun finishQuiz(){
+        val totalQuestions = questionModelList.size
+        val percentage = ((score.toFloat() / totalQuestions.toFloat()) *100).toInt()
+
+        val dialogBinding = ScoreDialogBinding.inflate(layoutInflater)
+        dialogBinding.apply {
+            scoreProgressIndicator.progress = percentage
+            scoreProgressText.text = "$percentage %"
+            if (percentage>50){
+                scoreTitle.text = "Congrats on passing the quiz! Well done!"
+                scoreTitle.setTextColor(Color.BLUE)
+            }else{
+                scoreTitle.text = "Sorry you didn't pass. Keep trying!"
+                scoreTitle.setTextColor(Color.RED)
+            }
+            scoreSubtitle.text = "$score out of $totalQuestions are correct."
+            finishBtn.setOnClickListener{
+                finish()
+            }
+        }
+        AlertDialog.Builder(this)
+            .setView(dialogBinding.root)
+            .setCancelable(false)
+            .show()
     }
 }
