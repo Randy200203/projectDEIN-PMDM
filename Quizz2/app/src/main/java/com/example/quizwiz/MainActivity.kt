a4576680daa2fe696bfc5e08f71caf6b2e805d2a
Randy.A <rachana@educacion.navarra.es>
2024-05-02 18:55:48 +0200

    Finishing

--- Quizz2/app/src/main/java/com/example/quizwiz/MainActivity.kt
+++ Quizz2/app/src/main/java/com/example/quizwiz/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.quizwiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.quizwiz.databinding.ActivityMainBinding
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -19,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupRecycleView(){
+        binding.progressBar.visibility = View.GONE
         adapter = QuizListAdapter(quizModellist)
         binding.recyclerViewItems.layoutManager = LinearLayoutManager(this)
         binding.recyclerViewItems.adapter = adapter
@@ -27,18 +30,38 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getDataFromFirebase(){
+        binding.progressBar.visibility = View.VISIBLE
         //dummydata
 
-        val listQuestionModel = mutableListOf<QuestionModel>()
-        listQuestionModel.add(QuestionModel("What is android?", mutableListOf("language", "OS","Product", "None"), "OS"))
-        listQuestionModel.add(QuestionModel("What is IOS?", mutableListOf("language", "Iphone","Product", "None"), "Iphone"))
-        listQuestionModel.add(QuestionModel("Best game?", mutableListOf("language", "Iphone","COD", "None"), "COD"))
+//        val listQuestionModel = mutableListOf<QuestionModel>()
+//        listQuestionModel.add(QuestionModel("What is android?", mutableListOf("language", "OS","Product", "None"), "OS"))
+//        listQuestionModel.add(QuestionModel("What is IOS?", mutableListOf("language", "Iphone","Product", "None"), "Iphone"))
+//        listQuestionModel.add(QuestionModel("Best game?", mutableListOf("language", "Iphone","COD", "None"), "COD"))
+//
+//
+//        quizModellist.add(QuizModel("1","Programming", "All the basic programming", "10",listQuestionModel))
+////        quizModellist.add(QuizModel("2","Computer", "All the basic computers", "20"))
+////        quizModellist.add(QuizModel("3","Gaming", "All the games", "15"))
+//
 
+          //Return the references of the database in Firebase
+        FirebaseDatabase.getInstance().reference
+            //returns all the list of Quiz models we have
+            .get()
+            .addOnSuccessListener { dataSnapShot ->
+                if (dataSnapShot.exists()){
+                    for (snapshot in dataSnapShot.children){
+                        //get each single quizmodel
+                        val quizModel = snapshot.getValue(QuizModel::class.java)
+                        if (quizModel != null) {
+                            //adding every quizmodel to our quizmodellist
+                            quizModellist.add(quizModel)
+                        }
+                    }
 
-        quizModellist.add(QuizModel("1","Programming", "All the basic programming", "10",listQuestionModel))
-//        quizModellist.add(QuizModel("2","Computer", "All the basic computers", "20"))
-//        quizModellist.add(QuizModel("3","Gaming", "All the games", "15"))
-       
-        setupRecycleView()
+                }
+                setupRecycleView()
+            }
+
     }
 }
