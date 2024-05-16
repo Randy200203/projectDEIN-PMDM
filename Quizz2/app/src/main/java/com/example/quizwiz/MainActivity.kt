3b9a9082c3928a8550a19fef55dca2fe7fcd3fc6
Randy.A <rachana@educacion.navarra.es>
2024-05-16 17:57:42 +0200

    changes

--- Quizz2/app/src/main/java/com/example/quizwiz/MainActivity.kt
+++ Quizz2/app/src/main/java/com/example/quizwiz/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.quizwiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.quizwiz.databinding.ActivityMainBinding
 import com.google.firebase.database.FirebaseDatabase
@@ -11,57 +14,64 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var quizModellist: MutableList<QuizModel>
     lateinit var adapter: QuizListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Find the toolbar and set it as the support action bar
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         quizModellist = mutableListOf()
         getDataFromFirebase()
     }
 
-    private fun setupRecycleView(){
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu_item, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item1 -> {
+                Toast.makeText(this@MainActivity, "Closing app...", Toast.LENGTH_SHORT).show()
+                finish() // This will close the activity
+            }
+
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setupRecycleView() {
         binding.progressBar.visibility = View.GONE
         adapter = QuizListAdapter(quizModellist)
         binding.recyclerViewItems.layoutManager = LinearLayoutManager(this)
         binding.recyclerViewItems.adapter = adapter
-
-
     }
 
-    private fun getDataFromFirebase(){
+    private fun getDataFromFirebase() {
         binding.progressBar.visibility = View.VISIBLE
-        //dummydata
+        // Dummy data commented out, assuming real data will be fetched from Firebase
 
-//        val listQuestionModel = mutableListOf<QuestionModel>()
-//        listQuestionModel.add(QuestionModel("What is android?", mutableListOf("language", "OS","Product", "None"), "OS"))
-//        listQuestionModel.add(QuestionModel("What is IOS?", mutableListOf("language", "Iphone","Product", "None"), "Iphone"))
-//        listQuestionModel.add(QuestionModel("Best game?", mutableListOf("language", "Iphone","COD", "None"), "COD"))
-//
-//
-//        quizModellist.add(QuizModel("1","Programming", "All the basic programming", "10",listQuestionModel))
-////        quizModellist.add(QuizModel("2","Computer", "All the basic computers", "20"))
-////        quizModellist.add(QuizModel("3","Gaming", "All the games", "15"))
-//
-
-          //Return the references of the database in Firebase
+        // Return the references of the database in Firebase
         FirebaseDatabase.getInstance().reference
-            //returns all the list of Quiz models we have
+            // Returns all the list of Quiz models we have
             .get()
-            .addOnSuccessListener { dataSnapShot ->
-                if (dataSnapShot.exists()){
-                    for (snapshot in dataSnapShot.children){
-                        //get each single quizmodel
+            .addOnSuccessListener { dataSnapshot ->
+                if (dataSnapshot.exists()) {
+                    for (snapshot in dataSnapshot.children) {
+                        // Get each single quiz model
                         val quizModel = snapshot.getValue(QuizModel::class.java)
                         if (quizModel != null) {
-                            //adding every quizmodel to our quizmodellist
+                            // Adding every quiz model to our quiz model list
                             quizModellist.add(quizModel)
                         }
                     }
-
                 }
                 setupRecycleView()
             }
-
     }
 }
